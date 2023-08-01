16c1ec09786a60ef18cf49d893c5a48479ccdbf0
greg-Da <gregoiredalmasso@gmail.com>
2023-08-01 10:41:40 +0200

    switch to psql

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem "faker", "~> 3.2"
+
+gem "pg", "~> 1.5"
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_27_153515) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
@@ -21,15 +24,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_153515) do
   create_table "gossips", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_gossips_on_user_id"
   end
 
   create_table "multi_pms", force: :cascade do |t|
-    t.integer "recipient_id"
-    t.integer "private_message_id"
+    t.bigint "recipient_id"
+    t.bigint "private_message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["private_message_id"], name: "index_multi_pms_on_private_message_id"
@@ -38,15 +41,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_153515) do
 
   create_table "private_messages", force: :cascade do |t|
     t.text "content"
-    t.integer "sender_id"
+    t.bigint "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["sender_id"], name: "index_private_messages_on_sender_id"
   end
 
   create_table "tag_gossips", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "gossip_id"
+    t.bigint "tag_id"
+    t.bigint "gossip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["gossip_id"], name: "index_tag_gossips_on_gossip_id"
@@ -65,7 +68,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_153515) do
     t.text "description"
     t.string "email"
     t.integer "age"
-    t.integer "city_id"
+    t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_users_on_city_id"
